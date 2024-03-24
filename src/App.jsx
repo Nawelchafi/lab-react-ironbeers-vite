ea29cb281112d124c19f6750d7e910551838dd42
Nawelchafi <chafinawel@gmail.com>
2024-03-24 16:04:19 +0100

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar'
+import HomePage from "./pages/HomePage"
+import AllBreersPage from "./pages/AllBeersPage"
+import AddBreerPage from "./pages/AddBeerPage"
+import BeerDetailsPage from "./pages/BeerDetailsPage"
+import RandomBreersPage from "./pages/RandomBeerPage"
+import AddBeerPage from "./pages/AddBeerPage";
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element=<HomePage /> ></Route>
+        <Route path="/beers" element=<AllBreersPage /> ></Route>
+        <Route path="/random-beer" element=<RandomBreersPage /> ></Route>
+        <Route path="/new-beer" element=<AddBeerPage /> ></Route>
+        <Route path="/beers/:beerId" element=<BeerDetailsPage /> ></Route>
+      </Routes>
     </div>
   );
 }
